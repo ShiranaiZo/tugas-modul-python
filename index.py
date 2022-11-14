50488da4158e335558f2b755e4e554dd471d3dff
Shiranai <faishalbariqmaulana123@gmail.com>
2022-11-14 22:47:38 +0700

    bug fixing and revision function jml_elemen_list, ismember, ismembers, ismemberls, max, max2 and add function isoneelmt

--- index.py
+++ index.py
@@ -5,7 +5,10 @@ def isEmpty(S):
     return S == []
 
 def jml_elemen_list(S):
-    return len(S)
+    if isinstance(S, int):
+        return len([S])
+    else:
+        return len(S)
 
 def is_atom(S):
     return not(is_empty_LoL(S)) and jml_elemen_list(S)==1
@@ -54,18 +57,28 @@ def IsEqs(S1,S2) :
             return IsEqs(first_List(S1),first_List(S2)) and IsEqs(tail_List(S1), tail_List(S2))
         else:
             return False
-        
+
+def IsMember(L, x):
+    if isEmpty(L):
+        return False
+    else:
+        if first_List(L)==x:
+            return True
+        else:
+            return IsMember(tail_List(L),x)
+
 def IsMemberS(A, S):
     if isEmpty(S):
         return False
     else:
-        if is_atom(S):
+        if is_atom(first_List(S)):
             return A == first_List(S)
-        elif is_list(S):
-            return IsMemberS(A, first_List(S)) or IsMemberS(A, tail_List(S))
+        elif is_list(first_List(S)):
+            return IsMember(first_List(S), A) or IsMemberS(A, tail_List(S))
+        else:
+            return False
             
 def isMemberLS(L, S):
-    print(L, S)
     if isEmpty(L) and isEmpty(S):
         return True
     elif (not isEmpty(L) and isEmpty(S)) or (isEmpty(L) and not isEmpty(S)):
@@ -78,6 +91,8 @@ def isMemberLS(L, S):
                 return True
             else:
                 return isMemberLS(L, tail_List(S))
+        else:
+            return False
         
 def Rember(a, S):
     if isEmpty(S):
@@ -89,18 +104,28 @@ def Rember(a, S):
             return Rember(a, tail_List(S))
         else:
             return konso_LoL(first_List(S), Rember(a, tail_List(S)))
-        
+
+def isOneElmt(L):
+    if not(isEmpty(L)):
+        return len(L) == 1
+ 
 def Max2(a,b):
     if a >= b:
         return a
     else:
-        b
-        
+        return b
+
 def Max(S):
-    if is_atom(first_List(S)):
-        return Max2(first_List(S), Max(tail_List(S)))
+    if isOneElmt(S):
+        if is_atom(first_List(S)):
+            return first_List(S)
+        else:
+            return Max(first_List(S))
     else:
-        return Max2(Max(first_List(S)), Max(tail_List(S)))
+        if is_atom(first_List(S)):
+            return Max2(first_List(S), Max(tail_List(S)))
+        else:
+            return Max2(Max([[first_List(S)]]), Max(tail_List(S)))
         
 
 # L1=[]
@@ -135,20 +160,21 @@ def Max(S):
 # print(last_List(X))
 # print(head_List(X))
 
-# S1 = [[2, 3]]
-# S2 = [[2, 3]] 
+# S1 = [['a', 'b', 28]]
+# S2 = [['a','b', 28]]
 # print(IsEqs(S1, S2))
-# print(len([1,2]))
 
-# A = 'd'
-# S = ['s', 'a', 'd'] 
+# A = 'b'
+# S = [[1, 'a', 'd'], ['c', 'b', 3]]
 # print(IsMemberS(A, S))
 
-# A = 'd'
-# S = ['s', 'a', 'd'] 
-# print(IsMemberS(A, S))
+# L = ['a', 'd', 1]
+# S = ['b', ['a', 'd', 1]] 
+# print(isMemberLS(L, S))
 
-L = ['a', 'd', 'c']
-S = ['b', ['a', 'd', 'c']] 
-print(isMemberLS(L, S))
-    
+# A = 3
+# S = [[1, 'b', 3], ['c', 'b', 3]]
+# print(Rember(A, S))
+
+# S = [[38, 50, 4, 6, 3]] 
+# print(Max(S))
